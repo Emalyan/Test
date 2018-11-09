47c20a2e56bc4f75f50374a2f8d96c18e89ac36a
Ivan <brandy.bg@mail.ru>
2018-11-10 00:24:32 +0300

    Hello

---
+++ ProgramOne/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = new int[96]; 
+            
+            
+            Random rand = new Random();
+
+            int i = 0;
+            
+
+            while (i < numbers.Length)
+            {
+
+                int random = rand.Next(0, 100);
+
+                if (!numbers.Contains(random))
+                {
+                    
+                    Console.Write("Поиск: ");                  
+                    numbers[i] = random;
+                    Console.WriteLine(i + " " + $"{ numbers[i]}");
+                    i++;
+                }
+               
+
+            }
+
+          
+
+            Console.ReadKey();
+        }
+
+       
+
+    }
+}
